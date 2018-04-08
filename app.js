24cd0b7467c583a7ae436b292007864939d879cf
raiyan106 <raiyan.khan.106@gmail.com>
2018-04-08 21:05:01 +0600

    Modification 1

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log("Starting the notes app");
+
+//Loading the Notes Module
+const notes = require('./notes.js');
+
+//Taking argumuments from the terminal
+const yargs = require('yargs');
+const args = yargs.argv
+const operation = args._[0]; //specifying the Note Operation
+let TITLE = args.title;
+let BODY = args.body;
+
+if(operation==='add')
+{
+    //console.log(`Adding notes with title ${args.title}`)
+    notes.addNotes(TITLE, BODY);
+
+} else if(operation==='update') {
+    notes.updateNote(TITLE, BODY);
+}
+
+ else if(operation=='remove'){
+    //console.log(`Removing note with title ${args.title}`)
+    notes.removeNote(args.title);
+} else
+{
+    console.log(`Invalid Operation`);
+}
--- notes.js
+++ notes.js
@@ -1,2 +1,69 @@
-console.log("Starting Notes.js file");
-console.log("Another Line");
+console.log("Notes.js required");
+const fs = require('fs');
+let addNotes = (title, body) =>{
+    //console.log(title, body);
+    let notes=[];
+    let note = {
+        title,
+        body
+    };
+    
+    try{
+        //let readNote = fs.readFileSync('notes.json');
+        notes = fs.readFileSync('notes.json');
+        notes = JSON.parse(notes);
+
+    } catch(e){}
+
+    let sameNotes=[];
+    notes.forEach(item => {
+        if(item.title===title)
+        {
+            sameNotes.push(item);
+        }
+    });
+
+    if(sameNotes.length===0){
+    notes.push(note);
+    fs.writeFileSync('notes.json',JSON.stringify(notes));
+    } else {
+        console.log("cannot add, same note exists\n\n");
+    }
+
+
+}
+
+let updateNote = (title, body) =>{
+    try {
+    let notes = fs.readFileSync('notes.json');
+    notes = JSON.parse(notes);
+    notes.forEach(item=>{
+        if(item.title===title){
+            item.body = body;
+            console.log(`The body of title \"${item.title}\" was changed to \"${body} \" `);
+            //notes.push(note);
+            fs.writeFileSync('notes.json',JSON.stringify(notes));
+            return;
+        }
+        console.log("Sorry, the Note was not found");
+        
+    });
+    } catch(e){
+        console.log("Notes database is empty, please create one!!");
+    }
+    
+    
+}
+
+let removeNote = (title) =>{
+    console.log(`Removing Note ${title}`);
+}
+
+module.exports = {
+    addNotes,
+    updateNote,
+    //readNote,
+    //getAllNotes,
+    removeNote
+}
+
