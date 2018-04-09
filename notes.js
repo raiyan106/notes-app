f25ff1b0b17388408b7f19ed939606298cff5513
raiyan106 <raiyan.khan.106@gmail.com>
2018-04-09 17:34:23 +0600

    updateNote has been defined

--- notes.js
+++ notes.js
@@ -35,24 +35,44 @@ let addNotes = (title, body) =>{
 
 let updateNote = (title, body) =>{
     try {
-    let notes = fs.readFileSync('notes.json');
-    notes = JSON.parse(notes);
-    notes.forEach(item=>{
+        let notes = fs.readFileSync('notes.json');
+         notes = JSON.parse(notes);
+    /*notes.forEach(item=>{
         if(item.title===title){
             item.body = body;
             console.log(`The body of title \"${item.title}\" was changed to \"${body} \" `);
             //notes.push(note);
             fs.writeFileSync('notes.json',JSON.stringify(notes));
             return;
+        }*/
+        for(let i=0;i<notes.length;i++){
+            if(notes[i].title===title){
+                notes[i].body = body;
+                console.log(`The body of title \"${notes[i].title}\" was changed to \"${body} \" `);
+                fs.writeFileSync('notes.json',JSON.stringify(notes));
+                return;
+            }
+              
         }
-        console.log("Sorry, the Note was not found");
-        
-    });
+        console.log("Sorry, the note was not found");
     } catch(e){
         console.log("Notes database is empty, please create one!!");
     }
     
     
+}
+
+let readNote = (title) =>{
+    try{
+        //let readNote = fs.readFileSync('notes.json');
+        let notes = fs.readFileSync('notes.json');
+        notes = JSON.parse(notes);
+        let noteFound = notes.filter(item=>{
+            return item.title===title;
+        })
+
+    } catch(e){}
+
 }
 
 let removeNote = (title) =>{
